600f5f7b11d4b4ce1b637b22b71e062b1aaedf83
srimidhuna <srimidhuna19@gmail.com>
2025-08-23 10:38:53 +0530

    initial commit

--- Day1.py
+++ Day1.py
@@ -4,11 +4,11 @@ print(a)
 
 #datatypes
 a=int(input("Enter a number:"))
-print(a)
+print(a,type(a))
 f=float(input("Enter a floating point number:"))
-print(f)
+print(f,type(f))
 b=input("Enter a string:")
-print(b)
+print(b,type(b))
 c=input("Enter a character: ")[0]
-print(c)
+print(c,type(c))
 
